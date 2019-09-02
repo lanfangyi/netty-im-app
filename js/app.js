ffa44a04519de10fbc5dae8ff654116d0aed5e57
lanfangyi <1069344911@qq.com>
2019-09-02 18:23:11 +0800

    完善代码

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ window.app = {
 	/**
 	 * http请求的地址
 	 */
-	serverUrl:'http://192.168.0.103:8080',
+	// serverUrl:'http://192.168.0.100:8080',
+	serverUrl:'http://10.4.34.4:8080',
 	
 	/**
 	 * websocket请求的地址
@@ -31,7 +32,27 @@ window.app = {
 	 * 获取用户的全局对象
 	 */
 	getGlobalUserInfo:function(){
-		return plus.storage.getItem("userInfo");
+		var userInfo = plus.storage.getItem("userInfo");
+		return JSON.parse(userInfo);
+	},
+	
+	setGlobalUserImg:function(userId, msgType, msg){
+		var msgList = plus.storage.getItem(userId+"_"+msgType);
+		//如果是空，则创建新的数组
+		if(msgList == null){
+			msgList = new Array();
+			msgList[0] = msg;
+			plus.storage.setItem(userId+"_"+msgType, msgList);
+		}else{
+			msgList.push(msg);
+			plus.storage.setItem(userId+"_"+msgType, msgList);
+		}
+	},
+	
+	//todo 先返回所有，后期改为分页
+	getGlobalUserImg:function(userId, msgType, pageId, pageSize){
+		var userInfo = plus.storage.getItem(userId+"_"+msgType);
+		return JSON.parse(userInfo);
 	}
 	
 }
